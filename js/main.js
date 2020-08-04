4138004df2613bc4035794b62ae6181f5fe9f482
auleki <kodagiwa@gmail.com>
2020-08-04 21:11:17 +0100

    all sections completed

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
 new WOW().init();
+
+const word = document.getElementById("word");
+const genesis = document.getElementById("genesis");
+
+word.addEventListener("click", () => {
+    genesis.scrollIntoView();
+})
