b2f6b6762473fe5c62aa70e4c7b5b693e7bcb834
auleki <kodagiwa@gmail.com>
2020-08-21 02:48:34 +0100

    timer added

--- js/quiz.js
+++ js/quiz.js
@@ -6,7 +6,10 @@ const game = document.getElementById("game");
 const progressText = document.getElementById("progressText");
 const scoreText = document.getElementById("score");
 const progressBarFull = document.getElementById("progressBarFull");
+const timeText = document.getElementById("time");
+
 // const 
+
 
 
 
@@ -19,12 +22,13 @@ const progressBarFull = document.getElementById("progressBarFull");
 let currentQuestion = {};
 let acceptingAnswers = false;
 let score = 0;
-questionCounterText
+// questionCounterText
+
 let questionCounter = 0;
 let availableQuestions = [];
 let questions = []
 
-// const baseURL = "https://opentdb.com/api.php?amount=3&category=27&difficulty=easy&type=multiple";
+const baseURL = "https://opentdb.com/api.php?amount=3&category=27&difficulty=easy&type=multiple";
 // const baseURL = "questions.json";
 // const baseURL = "https://raw.githubusercontent.com/auleki/checkadigs/master/questions.json";
 // fetch(baseURL)
@@ -35,47 +39,49 @@ let questions = []
 //   }).catch(e => console.log(e))
 
 
-fetch('questions.json')
+fetch(baseURL)
   // .then(res => res)
   // .then(data => console.log(data))
   // .catch(err => console.log(err))
 
-  .then(res => res)
+  .then(res => res.json())
   .then(data => {
-    const fetchedQuestions = data.url
+    const fetchedQuestions = data.results
     console.log(fetchedQuestions)
     // console.log(fetchedQuestions);
-    // questions = fetchedQuestions.map(loadedQuestion => {
-    //   const formattedQuestion = {
-    //     question: loadedQuestion.question
-    //   };
+    questions = fetchedQuestions.map(loadedQuestion => {
+      const formattedQuestion = {
+        question: loadedQuestion.question
+      };
 
-    //   const answerChoices = [...loadedQuestion.incorrect_answers];
-    //   formattedQuestion.answer = Math.floor(Math.random() * 3) * 1;
-    //   answerChoices.splice(
-    //     formattedQuestion.answer - 1, 
-    //     0,
-    //     loadedQuestion.correct_answer
-    //     );
+      const answerChoices = [...loadedQuestion.incorrect_answers];
+      formattedQuestion.answer = Math.floor(Math.random() * 3) * 1;
+      answerChoices.splice(
+        formattedQuestion.answer - 1, 
+        0,
+        loadedQuestion.correct_answer
+        );
         
-    //   answerChoices.forEach((choice, index) => {
-    //     formattedQuestion["choice" + (index + 1)] = choice;
-    //   });
+      answerChoices.forEach((choice, index) => {
+        formattedQuestion["choice" + (index + 1)] = choice;
+      });
 
-    //   return formattedQuestion;
+      return formattedQuestion;
 
-    // })
+    })
     
-    // startGame();
+    startGame();
   }).catch(e => console.log(e))
 
 
-const CORRECT_BONUS = 10;
-const MAX_QUESTIONS = 3;
+const CORRECT_BONUS = 1;
+const MAX_QUESTIONS = 5;
+let time = 10
 
 startGame = () => {
   questionCounter = 0;
   score = 0;
+  time = 16;  
   availableQuestions = [...questions];
   // console.log(availableQuestions);
   getNewQuestion();
@@ -83,7 +89,28 @@ startGame = () => {
   loader.classList.add("hidden");
 };
 
+// timeText.innerHTML = `${TIME} secs`
 
+// setInterval(() => {
+//   TIME == 0 ? getNewQuestion() : null
+//   TIME--
+//   timeText.innerHTML = `${TIME} secs`
+   
+//   // console.log(TIME)
+
+// }, 1000);
+
+
+// questionCounter++;
+const countDown = setInterval(() => {
+  time-- 
+  timeText.innerHTML = `${time} secs`
+  if (time == 0) {
+    time = 16
+    getNewQuestion()
+  }
+
+}, 1000);
 
 getNewQuestion = () => {
   // if(availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
@@ -92,45 +119,59 @@ getNewQuestion = () => {
   //   return console.log('no questions')
   // }
 
+  // TIME =  
+  // time = 15
+  // clearInterval(countDown);
   if(questionCounter >= MAX_QUESTIONS) {
     localStorage.setItem('mostRecentScore', score);
     return window.location.assign("/gameover.html");
     // return console.log('no questions')
   }
 
-  questionCounter++;
+  questionCounter++
   progressText.innerText = `Question: ${questionCounter}/${MAX_QUESTIONS}`
   // console.log((questionCounter / MAX_QUESTIONS) * 100);
   const quizProgress = `${(questionCounter / MAX_QUESTIONS)  * 100 }`
   // console.log(quizProgress);
   progressBarFull.style.width = `${quizProgress}%`
 
+  
   const questionIndex = Math.floor(Math.random() * availableQuestions.length);  
   currentQuestion = availableQuestions[questionIndex];
   question.innerHTML = currentQuestion.question;
-  
+ 
+
+  // timeText.innerHTML = 
 
   choices.forEach( choice => {
     const number = choice.dataset['number'];
-    choice.innerText = currentQuestion["choice" + number];    
+    choice.innerHTML = currentQuestion["choice" + number];    
+    
   });
+  // setInterval(() => {
+    
+  // }, 100);
 // console.log(availableQuestions);
 
   availableQuestions.splice(questionIndex, 1);
   acceptingAnswers = true;  
 };
 
+
+// CLICKING ON ANSWER 
+
 choices.forEach(choice => {
   choice.addEventListener('click', e => {
     if(!acceptingAnswers) return;
 
+    // timeText = time;
     acceptingAnswers = false;
     const selectedChoice = e.target;
     const selectedAnswer = selectedChoice.dataset["number"];
     const isCorrect = selectedAnswer == currentQuestion.answer;
 
     const classToApply = isCorrect ? 'correct' : 'incorrect';
-    console.log(classToApply.toString());
+    // console.log(classToApply.toString());
 
     if(classToApply === 'correct') {
       incrementScore(CORRECT_BONUS);
