6c9ee2206f567f19c463540a99b62aefb3510c84
auleki <kodagiwa@gmail.com>
2020-08-24 19:17:40 +0100

    newsletter field added

--- js/startWow.js
+++ js/startWow.js
@@ -5,36 +5,32 @@ const genesis = document.getElementById("genesis");
 const navbar = document.querySelector('.navbar');
 const burger = document.querySelector('.hamburger');
 
-// const navbarClass = navbar.className;
-// console.log(navbarClass);
 
 
 makeResponive = () => {
     let navClass = navbar.className;
-    // console.log(navClasses);
-    // if (x.className === "topnav") {
-    //     x.className += " responsive";
-    //   } else {
-    //     x.className = "topnav";
-    //   }
-
-    console.log(navClass);
-    if (navClass === 'navbar') {
-        console.log("MAKE RESPONSIVE");
+    if (navClass === 'navbar') {        
         navbar.className = `navbar responsive`
     } else {
-        console.log("ONLY NAVBAR");
-
         navbar.className = "navbar"
-    }
-    
+    }    
 }
 
 burger.addEventListener('click', makeResponive)
 
+
+// CLOSE MENU 
+
+
+
+
+
 word.addEventListener("click", () => {
     genesis.scrollIntoView();
 })
+
+
+
 
 $(document).ready(function() {
     $(".owl-carousel").owlCarousel({
